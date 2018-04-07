6a21323e2c934dd3f8acf28e53c22ccc0ff950ee
Chase Giunta <me@chasegiunta.com>
2018-04-07 11:58:28 -0500

    Add Tailwind

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,9 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-
+// https://github.com/adamwathan/vue-cli-tailwind-example/edit/master/.postcssrc.js
 module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
+  "plugins": [
+    require('postcss-import')(),
+    require('postcss-url')(),
+    require('tailwindcss')('./tailwind.js'),
+    require('autoprefixer')(),
+  ]
 }
