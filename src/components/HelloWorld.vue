c316ad38df1d6260172bde5df451f365592509cf
Chase Giunta <me@chasegiunta.com>
2019-06-07 00:10:55 -0500

    Update Tailwind to ^1.0.0

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
-    <img src="/assets/logo.png" />
-    <h1 class="mb-4">{{ msg }}</h1>
+    <img class="mx-auto" src="/assets/logo.png" />
+    <h1 class="text-3xl mb-4">{{ msg }}</h1>
     <p class="mb-3">
       You're viewing the <code>&#060;HelloWorld&#062;</code> component!
     </p>
@@ -12,19 +12,19 @@
     <p class="mb-3">Passing in some data from twig:</p>
 
     <div
-      class="mt-3 p-4 inline-block rounded-lg leading-loose bg-black text-left"
+      class="mt-3 mb-10 p-4 inline-block rounded-lg leading-loose bg-black text-left"
     >
-      <code class="text-grey">
+      <code class="text-gray-500">
         <span class="text-white">Craft Version: </span>
-        <span class="text-green-light">{{ info.craftVersion }}</span> (passed in
+        <span class="text-green-400">{{ info.craftVersion }}</span> (passed in
         as prop)<br />
 
         <span class="text-white">Environment: </span>
-        <span class="text-green-light">{{ info.environment }}</span> (passed in
-        as prop)<br />
+        <span class="text-green-400">{{ info.environment }}</span> (passed in as
+        prop)<br />
 
         <span class="text-white">DB Name: </span>
-        <span class="text-green-light"><slot></slot></span> (passed in through
+        <span class="text-green-400"><slot></slot></span> (passed in through
         slot)
       </code>
     </div>
