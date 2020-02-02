a47ece24790d46be862812c88a0666a5b741dd77
Chase Giunta <me@chasegiunta.com>
2020-02-02 11:37:56 -0600

    Remove scoped styles & add caveat comment about Purge

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -44,7 +44,8 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<!-- @apply classes may be incorrectly purged if "scoped" is present -->
+<style>
 .hello {
   @apply text-white;
 }
